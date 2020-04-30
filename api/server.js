2a35366d1afbcbc1da6fd62cba1a43640e621e1b
CodyBrewer <codybrewerdev@gmail.com>
2020-04-30 16:03:02 -0600

    add cors and helmet middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
 
 app.get('/api/status', (req, res) => {
   res.send({ api: 'up' });
