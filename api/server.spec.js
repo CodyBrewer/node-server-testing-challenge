eea187efb71cc58afe45099c647ad271e90cd6d8
CodyBrewer <codybrewerdev@gmail.com>
2020-05-01 09:21:18 -0600

    write test for api/characters/status

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server.js', () => {
-  describe('GET /status', () => {
+  describe('GET /api/status', () => {
     it('should return 200 OK', async () =>{
       const response = await request(server).get('/api/status');
       expect(response.status).toBe(200);
@@ -13,10 +13,27 @@ describe('server.js', () => {
 
       expect(response.type).toMatch(/json/i);
     });
-    it('should return { api: \'up\'}', async () => {
+    it('should return { api: \'up\' }', async () => {
       const response = await request(server).get('/api/status');
 
       expect(response.body.api).toBe('up');
+    });
+  });
+  describe('GET /api/characters/status', () => {
+    it('should return 200 OK', async () => {
+      const response = await request(server).get('/api/characters/status');
+
+      expect(response.status).toBe(200);
+    });
+    it('should return json', async () => {
+      const response = await request(server).get('/api/characters/status');
+
+      expect(response.type).toMatch(/json/i);
+    });
+    it('should return { characterRouter: \'up\' }', async () => {
+      const response = await request(server).get('/api/characters/status');
+
+      expect(response.body.characterRouter).toBe('up');
     });
   });
 });
