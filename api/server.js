0523d9f050b27b417daf0f2577a6372bd6bacb19
CodyBrewer <codybrewerdev@gmail.com>
2020-05-01 09:27:31 -0600

    add charactersRouter to routes, passing /api/characters/status test

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const charactersRouter = require('./characters/router.js');
 
 const app = express();
 
+// Global Middleware
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
+
+// Routes
+app.use('/api/characters', charactersRouter);
 
 app.get('/api/status', (req, res) => {
   res.send({ api: 'up' });
